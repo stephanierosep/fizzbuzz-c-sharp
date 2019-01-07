991b45f4f82950b8db8978ddb7e74c184658680a
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2019-01-07 14:30:18 +0000

    Working fizzbuzz

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+     
+        {
+            bool isDivisibleByThree;
+            bool isDivisibleByFive;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                isDivisibleByThree = (i % 3 == 0);
+                isDivisibleByFive = (i % 5 == 0);
+
+                if (isDivisibleByThree && isDivisibleByFive)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (isDivisibleByThree) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (isDivisibleByFive)
+                { 
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
